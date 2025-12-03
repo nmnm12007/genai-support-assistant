0baa86e1c6d5e585cc702bf394bd4259627246d2
n.maheshwar <nmnm.1.2007@gmail.com>
2025-12-03 15:22:39 +0530

    flask template using HTTP GET,POST, and code using redirect(url_for=<methodname>, )

--- HTTP_Flask.py
+++ HTTP_Flask.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -11,9 +11,19 @@ def home():
 def about():
     return render_template("child.html")
 
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    render_template("http_flask.html")
+@app.route("/logininfo", methods=["GET", "POST"])
+def logininfo():
+    if request.method == "GET":
+        return render_template("logininfo.html")
+    elif request.method == "POST":
+        htmlname_loc = request.form["htmlname"]
+        return redirect(url_for("methoduser", username=htmlname_loc))
+    return None
+
+
+@app.route("/<username>")
+def methoduser(username):
+    return f"<h1>{username}</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
